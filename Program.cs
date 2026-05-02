ea5729ad44ead7000e512d552acebc614c7534a9
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2026-05-02 09:29:29 +0500

    feat: implement arifmetic moddule

--- Program.cs
+++ Program.cs
@@ -12,6 +12,98 @@ switch(menuOption)
 			Console.Write("Tanlovni kiriting: ");
 			string? arifmeticOption = Console.ReadLine();
 			
+			switch(arifmeticOption)
+      {
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "9":
+          {
+            Console.Write("\nBirinchi sonni kiriting: ");
+            string? firstString = Console.ReadLine();
+            decimal firstNumber = Convert.ToDecimal(firstString);
+            Console.Write("Ikkinchi sonni kiriting: ");
+            string? secondString = Console.ReadLine();
+            decimal secondNumber = Convert.ToDecimal(secondString);
+
+            switch(arifmeticOption)
+            {
+              case "1":
+                {
+                  Console.WriteLine($"\n{firstNumber} + {secondNumber} = {firstNumber+secondNumber}");
+                  break;
+                }
+              case "2":
+                {
+                  Console.WriteLine($"\n{firstNumber} - {secondNumber} = {firstNumber-secondNumber}");
+                  break;
+                }
+              case "3":
+                {
+                  Console.WriteLine($"\n{firstNumber} * {secondNumber} = {firstNumber*secondNumber}");
+                  break;
+                }
+              case "4":
+                {
+                  Console.WriteLine($"\n{firstNumber} / {secondNumber} = {firstNumber/secondNumber}");
+                  break;
+                }
+              case "9":
+                {
+                  Console.WriteLine($"\n{firstNumber} % {secondNumber} = {firstNumber%secondNumber}");
+                  break;
+                }
+            }
+            break;
+          }
+        case "5":
+        case "6":
+        case "7":
+          {
+            Console.Write("\nSonni kiriting: ");
+            string? firstString = Console.ReadLine();
+            double firstNumber = Convert.ToDouble(firstString);
+
+            switch (arifmeticOption)
+            {
+              case "5":
+                {
+                  Console.WriteLine($"\n{firstNumber} sonining kvadrati: {Math.Pow(firstNumber, 2)}");
+                  break;
+                }
+              case "6":
+                {
+                  Console.WriteLine($"\n{firstNumber} sonining kubi: {Math.Pow(firstNumber, 3)}");
+                  break;
+                }
+              case "7":
+                {
+                  Console.WriteLine($"\n{firstNumber} sonining ikkilik ildizi: {Math.Sqrt(firstNumber)}");
+                  break;
+                }
+            }
+            break;
+          }
+        case "8":
+          {
+            Console.Write("\nSonni kiriting: ");
+            string? firstString = Console.ReadLine();
+            double firstNumber = Convert.ToDouble(firstString);
+            Console.Write("Darajani kiriting: ");
+            string? secondString = Console.ReadLine();
+            double secondNumber = Convert.ToDouble(secondString);
+
+            Console.WriteLine($"\n{firstNumber} sonining {secondNumber}-darajasi: {Math.Pow(firstNumber, secondNumber)}");
+            break;  
+          }
+        default:
+          {
+            Console.WriteLine("\nNoto'g'ri buyruq kiritdingiz! Iltimos dasturni qayta ishga tushiring.");
+            break;
+          }
+      }
+
 			break;	
 		}
 	case "2":
