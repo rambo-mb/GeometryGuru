8b3cfe1c27a90e4f30261aff5a4492cad6a21735
Izzatjon Qodirov <qodirov.izzatjon.rambo.mb@gmail.com>
2026-05-02 09:27:38 +0500

    feat: add main menu

--- Program.cs
+++ Program.cs
@@ -1,18 +1,36 @@
-﻿Console.WriteLine("Geron formulasi yordamida uchburchak yuzasini \nhisoblovchi dasturga xush kelibsiz!");
+﻿Console.WriteLine("\n=== GeometryGuru v2 ===\n");
+Console.WriteLine("1. Arifmetik amallar \n2. Geometrik amallar \n0. Chiqish \n");
+Console.Write("Tanlovni kiriting: ");
+string? menuOption = Console.ReadLine();
 
-Console.Write("Uchburchak a tomoni: ");
-string? aSideStr = Console.ReadLine();
-int aSide = Convert.ToInt32(aSideStr);
+switch(menuOption)
+{
+	case "1":
+		{
+			Console.WriteLine("\n=== Arifmetik amallar ===\n");
+			Console.WriteLine("1. Qo‘shish \n2. Ayirish \n3. Ko‘paytirish \n4. Bo‘lish \n5. Kvadrat \n6. Kub \n7. Kvadrat ildiz \n8. Darajaga oshirish \n9. Qoldiq\n");
+			Console.Write("Tanlovni kiriting: ");
+			string? arifmeticOption = Console.ReadLine();
+			
+			break;	
+		}
+	case "2":
+		{
+			Console.WriteLine("\n=== Geometrik amallar ===\n");
+			Console.WriteLine("1. Uchburchak yuzasi — Heron formulasi \n2. Aylana uzunligi va yuzasi \n3. Shar hajmi \n4. Silindr hajmi \n5. Konus hajmi \n6. Pifagor teoremasi\n");
+			Console.Write("Tanlovni kiriting: ");
+			string? geometricOption = Console.ReadLine(); 
 
-Console.Write("Uchburchak b tomoni: ");
-string? bSideStr = Console.ReadLine();
-int bSide = Convert.ToInt32(bSideStr);
-
-Console.Write("Uchburchak c tomoni: ");
-string? cSideStr = Console.ReadLine();
-int cSide = Convert.ToInt32(cSideStr);
-
-int s = (aSide + bSide + cSide) / 2;
-decimal areaTriangle = (decimal) Math.Sqrt(s * (s - aSide) * (s - bSide) * (s - cSide));
-
-Console.WriteLine($"Uchburchak yuzi: {areaTriangle}");
+			break;
+		}
+	case "0":
+		{
+			Console.WriteLine("\nDasturdan foydalanganingiz uchun rahmat. Ko'rishguncha!");
+			break;
+		}
+	default:
+		{
+			Console.WriteLine("\nNoto'g'ri buyruq kiritdingiz! Iltimos dasturni qayta ishga tushiring.");
+			break;
+		}
+}
